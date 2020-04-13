47e9822c54a0659cc21bcecac6a24d5075b27a84
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-04-13 18:59:42 -0400

    Clean up and timeStamp

--- gmail/bigQuery/bigquery.py
+++ gmail/bigQuery/bigquery.py
@@ -1,29 +1,33 @@
 from google.cloud import bigquery
 import os
-
+import re
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "credentials/proj.json"
+
 
 class BigQ:
 
     def __init__(self):
         self.client = bigquery.Client()
 
-    def insert(self,id,ret,msg):
-        # Construct a BigQuery client object.
+    def clean(self, s):
+        return re.sub(r'\W+', ' ', s)
 
-        id = id.replace("'", "~")
-        ret = ret.replace("'", "~")
-        msg = msg.replace("'", "~")
+    def getTime(self):
+        query = "SELECT CURRENT_DATETIME('America/New_York') as now;"
+        query_job = self.client.query(query)
+        for row in query_job:
+            # Row values can be accessed by field name or index.
+            return row["now"]
+
+    def insert(self, id, ret, msg):
+        id = self.clean(id)
+        msg = self.clean(msg)
 
         query = """
-          insert into `septapig.mail.mc` (id,returnpath,msg) 
-          values ('%s','%s','%s')
-          """ % (id,ret,msg)
+          insert into `septapig.mail.mc` (id,returnpath,msg,timeStamp) 
+          values ('%s','%s','%s','%s')
+          """ % (id, ret, msg,self.getTime())
 
         query_job = self.client.query(query)  # Make an API request.
-        print("result:",query_job)
-
-
-
-
+        print("result:", query_job)
