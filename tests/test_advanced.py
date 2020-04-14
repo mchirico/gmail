76d00595c43f240248aa6bcec2dfbcea8ec942f5
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-04-14 15:12:25 -0400

    Adding fixtures

--- tests/test_advanced.py
+++ tests/test_advanced.py
@@ -17,11 +17,24 @@ class AdvancedTestSuite(TestCase):
         j = Junk()
         self.assertEqual("we stuff", j.stuff())
 
+    def test_pickle(self):
+        m = Mail()
+        a = '234'
+        m.pickle_it("fixtures/obj.pkl", a)
+        r = m.unpickle_it("fixtures/obj.pkl")
+        self.assertEqual(a, r)
+
+    def test_response(self):
+        m = Mail()
+        r = m.unpickle_it("fixtures/message.pkl")
+        print(r)
+        self.assertEqual(r['id'], '1717a15dc100f5bd')
+
     def test_Mail(self):
         m = Mail()
         q = BigQ()
         m.main()
         for i in m.data:
-            q.insert(i[0],i[1],i[2],i[4])
+            q.insert(i[0], i[1], i[2], i[4])
 
         # self.assertEqual("we stuff", j.stuff())
