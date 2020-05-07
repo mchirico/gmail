ed2dbde8b882252252d0baf17e4fd2053ec75a59
Mike Chirico <mchirico@gmail.com>
2020-05-07 08:27:12 -0400

    Rejects (#18)

--- gmail/rejects/rejects.py
+++ gmail/rejects/rejects.py
@@ -1,12 +1,25 @@
+import re
+from gmail.rejects.listing import rejects
+
+
 class Rejects:
     mylist = ['found', 'human', 'reject', '<>', 'deegit.com',
               'tech.com', 'mail.net', 'talent', 'solutions',
               'apolisrises.com', 'sparkpost.com', 'beyond.com', 'patel',
               'solutions.com', 'smtp.com', 'kumar', 'salesforce.com',
-              'providers.com','spectrum.com','kodus.com']
+              'providers.com','spectrum.com','kodus.com','sparkpost.com']
+
+    def match(self, returnpath):
+        m = re.search(r"(@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)", returnpath)
+        return m
+
 
     def returnpath(self, returnpath):
         for i in self.mylist:
             if i in str(returnpath).lower():
                 return True
+            m = self.match(returnpath)
+            if m:
+                if m.group(0) in rejects:
+                    return True
         return False
--- tests/test_rejects.py
+++ tests/test_rejects.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from gmail.rejects.rejects import Rejects
+from gmail.rejects.listing import rejects
 
 from unittest import TestCase
 
@@ -12,3 +13,12 @@ class RejectsTestSuite(TestCase):
         r = Rejects()
         test = 'Return-Path: <Akshit@ustechsolutionsinc.com>'
         self.assertTrue(r.returnpath(test))
+
+    def test_match(self):
+        r = Rejects()
+        self.assertFalse(r.returnpath('bob@gmail.com'))
+        self.assertTrue(r.returnpath('bob@compugain.com'))
+
+    def test_listing(self):
+        r = rejects
+        self.assertTrue('@pyramidci.com') in r
